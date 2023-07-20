d3f049d51993ba1e4997b32608143e6d427e5d23
vlduggen <vitor.duggen@amaiseducacao.com.br>
2023-07-20 18:22:49 -0300

    Configurado eslint/prettier

--- init.lua
+++ init.lua
@@ -52,6 +52,50 @@ return {
     servers = {
       -- "pyright"
     },
+    plugins = {
+--      init = {
+--        ["Darazaki/indent-o-matic"] = { disable = true },
+--      },
+      {
+        "jay-babu/mason-null-ls.nvim",
+        opts = {
+          handlers = {
+            -- for prettier
+            prettier = function()
+              require("null-ls").register(require("null-ls").builtins.formatting.prettier.with {
+                condition = function(utils)
+                  return utils.root_has_file "package.json"
+                    or utils.root_has_file ".prettierrc"
+                    or utils.root_has_file ".prettierrc.json"
+                    or utils.root_has_file ".prettierrc.js"
+                end,
+              })
+            end,
+            -- for prettierd
+            prettierd = function()
+              require("null-ls").register(require("null-ls").builtins.formatting.prettierd.with {
+                condition = function(utils)
+                  return utils.root_has_file "package.json"
+                    or utils.root_has_file ".prettierrc"
+                    or utils.root_has_file ".prettierrc.json"
+                    or utils.root_has_file ".prettierrc.js"
+                end,
+              })
+            end,
+            -- For eslint_d:
+            eslint_d = function()
+              require("null-ls").register(require("null-ls").builtins.diagnostics.eslint_d.with {
+                condition = function(utils)
+                  return utils.root_has_file "package.json"
+                    or utils.root_has_file ".eslintrc.json"
+                    or utils.root_has_file ".eslintrc.js"
+                end,
+              })
+            end,
+          },
+        },
+      },
+    }
   },
 
   -- Configure require("lazy").setup() options
